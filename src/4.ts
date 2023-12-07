8288ea80b4539df7855958e4571e9bcd4cad2f57
Dima Tytenko <amgpetronass@gmail.com>
2023-12-07 13:25:58 +0200

    added hw

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,55 @@
+class Key {
+  private signature = Math.random()
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey() {
+    return this.key;
+  }
+}
+
+class House {
+  tenants: Person[] = [];
+
+  constructor(public door: boolean, public key: Key) {
+    this.door = door;
+  }
+  
+  comeIn(person: Person) {
+    if (this.door === true) {
+      this.tenants.push(person);
+      console.log('Welcome!');
+    } else {
+      console.log('You can not come in!');
+    }
+  }
+
+  openDoor(key: Key) {
+  this.key = key;
+  }
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(false, key);
+  }
+
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('Door is open!');
+    } else {
+      console.log('You can not open the door!');
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
